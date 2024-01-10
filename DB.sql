018bac0700177f7f97a8b89bc2f7e1650a4b7df4
fake150907 <147607275+fake150907@users.noreply.github.com>
2024-01-10 16:57:20 +0900

    회원가입 기능 추가, 아이디 중복 체크, 비밀번호 체크, 필수 입력 정보

--- DB.sql
+++ DB.sql
@@ -10,18 +10,33 @@ CREATE TABLE article(
     `body` TEXT NOT NULL
 );
 
+CREATE TABLE `member`(
+	id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	regDate DATETIME NOT NULL,
+	updateDate DATETIME NOT NULL,
+	loginId VARCHAR(100) NOT NULL UNIQUE,
+	loginPw VARCHAR(100) NOT NULL,
+	`name` VARCHAR(100) NOT NULL
+);
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = '수달',
+loginPw = 'asd123',
+`name` = '고수달';
+
+SELECT *
+FROM `member`;
 SHOW TABLES;
 
 SELECT *
 FROM article
 ORDER BY id DESC;
 
-
 SELECT *
 FROM article
 ORDER BY id DESC;
-
-INSERT INTO article SET regDate = NOW(),updateDate = NOW(),title = CONCAT('제목', RAND()),`body` = CONCAT('내용', RAND());
 
 INSERT INTO article
 SET regDate = NOW(),
--- src/com/KoreaIT/java/JDBCAM/App.java
+++ src/com/KoreaIT/java/JDBCAM/App.java
@@ -63,6 +63,81 @@ public class App {
 			return -1;
 		}
 
+		if (cmd.equals("member join")) {
+			System.out.println("==회원 가입==");
+			String loginId = null;
+			while (true) {
+				System.out.print("로그인 아이디 : ");
+				loginId = sc.nextLine().trim();
+
+				if (loginId.length() < 2) {
+					System.out.println("아이디를 두 글자 이상 입력해주세요.");
+					continue;
+				}
+
+				if (loginId.contains(" ")) {
+					System.out.println("공백을 포함하면 안됩니다.");
+					continue;
+				}
+
+				SecSql sql = new SecSql();
+				sql.append("SELECT COUNT(*) > 0");
+				sql.append("FROM `member`");
+				sql.append("WHERE loginId = ?;", loginId);
+
+				boolean isLoginIdDup = DBUtil.selectRowBooleanValue(conn, sql);
+
+				if (isLoginIdDup) {
+					System.out.println("이미 있는 아이디입니다 다시입력해주세요.");
+					continue;
+				} else {
+					System.out.println("사용할 수 있는 아이디입니다.");
+					break;
+				}
+			}
+
+			String loginPw = null;
+			while (true) {
+				System.out.print("로그인 비밀번호 : ");
+				loginPw = sc.nextLine().trim();
+
+				if (loginPw.length() < 2) {
+					System.out.println("비밀번호를 두 글자 이상 입력해주세요");
+					continue;
+				}
+
+				if (loginPw.contains(" ")) {
+					System.out.println("공백을 포함하면 안됩니다.");
+					continue;
+				}
+
+				System.out.print("로그인 비밀번호 확인 : ");
+				String loginPwConfirm = sc.nextLine();
+				if (!loginPw.equals(loginPwConfirm)) {
+					System.out.println("비밀번호가 일치하지 않아. 다시 입력해주세요.");
+					continue;
+				}
+				break;
+			}
+
+			System.out.print("이름 : ");
+			String name = sc.nextLine();
+
+			SecSql sql = new SecSql();
+
+			sql.append("INSERT INTO `member`");
+			sql.append("SET regDate = NOW(),");
+			sql.append("updateDate = NOW(),");
+			sql.append("loginId = ?,", loginId);
+			sql.append("loginPw = ?,", loginPw);
+			sql.append("`name` = ?;", name);
+
+			int id = DBUtil.insert(conn, sql);
+
+			System.out.printf("%d번 회원이 가입 되었습니다. %s님 환영합니다.\n", id, name);
+
+		}
+
 		if (cmd.equals("article write")) {
 			System.out.println("==글쓰기==");
 			System.out.print("제목 : ");
@@ -223,4 +298,5 @@ public class App {
 
 		return 0;
 	}
+
 }
